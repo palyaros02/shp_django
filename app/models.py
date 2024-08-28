b609fa4bb610bbffd9ccc12676466cef1dad3751
Ярослав Паламарчук <palyaros02@mail.ru>
2024-08-28 15:36:02 +1000

    added Profile

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True, blank=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.full_name
