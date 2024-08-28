45cc1ecfee23076317fa8e427d2ad6b56848495f
Ярослав Паламарчук <palyaros02@mail.ru>
2024-08-28 21:05:16 +1000

    fixed settings and urls

--- news_website/urls.py
+++ news_website/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from app import views
 
@@ -30,4 +32,4 @@ urlpatterns = [
 
     # Маршрут к профилю
     path('profile/', views.profile_view, name='profile'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
