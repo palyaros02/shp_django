90bf0b9f991ee8a187c5ef03f6f04af77fefd869
Ярослав Паламарчук <palyaros02@mail.ru>
2024-08-29 07:02:31 +1000

    added requirements

--- news_website/urls.py
+++ news_website/urls.py
@@ -1,9 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, re_path
-from django.views.static import serve
-from django.views.generic import RedirectView
+from django.urls import path
 
 from app import views
 
