d7e25224ca31f58a5110e48c362ef0a5fbd0efad
Ярослав Паламарчук <palyaros02@mail.ru>
2024-08-28 15:36:41 +1000

    added Comment

--- app/models.py
+++ app/models.py
@@ -23,3 +23,12 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    news = models.ForeignKey(News, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Комментарий пользователя {self.author.full_name} к статье "{self.news.title}"'
